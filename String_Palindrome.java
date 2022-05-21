5ae0dcd18a93ea1e116e8aea8dc15be02da14b21
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2022-05-21 14:01:15 +0530

    Create String_Palindrome.java

---
+++ String_Palindrome.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main
+{
+    public static boolean isPalindrome(String str) {
+        for(int i = 0; i < str.length() / 2; i++) {
+            if(str.charAt(i) != str.charAt(str.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+	public static void main(String[] args) {
+        String str = "Palindrome";
+        
+        if(isPalindrome(str)){
+            System.out.println("String is a Palindrome");
+        }
+        else {
+            System.out.println("String is not a Palindrome");
+        }
+	}
+}
